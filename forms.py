3173554ae058fde5c819d59741e3521dd5351bfc
Nikita-333 <krawchenko.nikita2016@gmail.com>
2023-10-13 02:48:26 +0300

    update class Registration and Login

--- forms.py
+++ forms.py
@@ -7,7 +7,9 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')

7eed1059b06dce78f8d448f8a1d8315af51a98a0
Nikita-333 <krawchenko.nikita2016@gmail.com>
2023-10-31 03:27:24 +0300

    Add realization upload file for user

--- model.py
+++ model.py
@@ -1,11 +1,18 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-
+#/home/rydyar/cours/cur
 db = SQLAlchemy()
 class User(db.Model,UserMixin):
-    tablename = 'users'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     nickname = db.Column(db.String(80), unique=True, nullable=False)
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    filename = db.Column(db.String(), nullable=False)

ce8950a25839b76391bad27bd1f7cc7836bda52e
Nikita-333 <krawchenko.nikita2016@gmail.com>
2023-10-31 03:26:09 +0300

    Add forms for upload file

--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
+from flask_uploads import UploadSet, IMAGES
+from model import Image
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
+photos = UploadSet('photos',IMAGES)
 class RegistrationForm(FlaskForm):
     nickname = StringField('Nickname', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -13,3 +17,13 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class FileUploadForm(FlaskForm):
+
+    file = FileField(
+        validators=[
+            FileRequired('File field should not be empty'),
+            FileAllowed(photos,['jpg','png'])
+        ]
+    )
+    submit = SubmitField('Upload')
